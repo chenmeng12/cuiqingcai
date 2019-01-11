55e80da377bd7242a93b0300124a9f34c0fd72ba
chen <1094955329@qq.com>
2019-01-11 21:46:01 +0800

    提取URL

--- Ajax/toutiao.py
+++ Ajax/toutiao.py
@@ -1,8 +1,30 @@
 import requests
 from urllib import parse
+import json
 
 def get_page(offset):
     params = {
         'offset':offset,
+        'format':'json',
+        'keyword':'街拍',
+        'autoload':'true',
+        'count':20,
+        'cur_tab':1,
+        'from':'search_tab',
+        'pd':'synthesis'
 
     }
+    url = 'https://www.toutiao.com/search_content/?' + parse.urlencode(params)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+    except ConnectionError:
+        return None
+
+
+if __name__ == '__main__':
+    for item in get_page(0)['data']:
+        title = item.get('title')
+        for image in item['image_list']:
+            print(image['url'])
