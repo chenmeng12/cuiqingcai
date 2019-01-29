d0b5baab1fbd93af1ee4fabeb94e6b4f9aeea00f
chen <1094955329@qq.com>
2019-01-29 14:50:12 +0800

    1.抓取淘宝

--- dongtaixuanran/taobao.py
+++ dongtaixuanran/taobao.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from urllib.parse import quote
+brower = webdriver.Chrome()
+keyword = 'iPad'
+wait = WebDriverWait(brower, 10)
 
+url = 'https://s.taobao.com/search?q='+quote(keyword)
+
+brower.get(url)
